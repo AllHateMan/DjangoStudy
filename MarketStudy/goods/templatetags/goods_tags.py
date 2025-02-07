e473b8cb83d03e7259d11adc45c9f0bbba90bdfe
AllHateMan <dimavybornov@gmail.com>
2025-02-07 14:03:39 +0100

    Simple search done

--- MarketStudy/goods/templatetags/goods_tags.py
+++ MarketStudy/goods/templatetags/goods_tags.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlencode
 from django import template
 
 from goods.models import Categories
@@ -6,4 +7,10 @@ register = template.Library()
 
 @register.simple_tag()
 def category_list():
-    return Categories.objects.all()   
+    return Categories.objects.all() 
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwards):
+    querry = context['request'].GET.dict()
+    querry.update(kwards)
+    return urlencode(querry)
--- MarketStudy/goods/views.py
+++ MarketStudy/goods/views.py
@@ -1,19 +1,34 @@
+
 from django.shortcuts import get_list_or_404, render
 from django.core.paginator import Paginator
 
+
+from goods.utils import q_search
 from goods.models import Product
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
 
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         product = Product.objects.all()
+    elif query:
+        product = q_search(query)
+        
     else :
         product = get_list_or_404 (Product.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        product = product.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        product = product.order_by(order_by)
     
-    paginator = Paginator(product, 3)
+    paginator = Paginator(product, 10)
     current_page = paginator.page(int(page))
 
     context = {
@@ -23,7 +38,7 @@ def catalog(request, category_slug):
         'slug_url': category_slug
     } 
 
-    return render(request, 'goods/catalog.html', context);
+    return render(request, 'goods/catalog.html', context)
 
 
 def product(request, product_slug):
