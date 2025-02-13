667442f5d5cdc333542f1a9d07d05b8a92488de8
AllHateMan <dimavybornov@gmail.com>
2025-02-13 14:26:25 +0100

    Remove product added

--- MarketStudy/carts/templatetags/carts_tags.py
+++ MarketStudy/carts/templatetags/carts_tags.py
@@ -6,5 +6,6 @@ register = template.Library()
 
 @register.simple_tag()
 def users_carts(request):
+    """Повертає кошик користувача"""
     if request.user.is_authenticated:
         return Cart.objects.filter(user=request.user)
